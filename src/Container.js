d30cfb9de22c94ffe7515f866858dfe08667b708
Juan Vargas-Murillo <juanvmurillo@gmail.com>
2017-07-26 20:26:57 -0600

    added server farm images

--- src/Container.js
+++ src/Container.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import App from './App.js';
 
-var farmPics = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiDyFcvz5PhzQxuiFseY1-X4lNYBHFyXkB1uQe7nzgTFnZBalcVQ", "http://www.mountainviewgrand.com/files/2794/Farm_summer_1.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAqZ-whO2rLlkS8w7BIF3Ztft3o3gW0yJ12uVQl47jU4b7GkcC"];
+var farmPics = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiDyFcvz5PhzQxuiFseY1-X4lNYBHFyXkB1uQe7nzgTFnZBalcVQ", "http://www.mountainviewgrand.com/files/2794/Farm_summer_1.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAqZ-whO2rLlkS8w7BIF3Ztft3o3gW0yJ12uVQl47jU4b7GkcC", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD4j4MZWI9Zr_bjbKmnjr1CzECARhp6gY-T0th2qbkRNdsLZ55IQ", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3TDK1-GjVsHYuSBIx5AEhe7UyTmgrUkgCJYTg0O5xQeRO3ldwVQ", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoLrgyeaW-C2BFF1ColZ1OIUZactWk7rTvJgSdPt6imVL2VAuHSw"];
 
 function getRandomFarm(imgAr){
   var num = Math.floor( Math.random() * imgAr.length );
